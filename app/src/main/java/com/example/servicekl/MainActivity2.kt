ee6127fa48b219629d07cbdd2d1a9421aaaf54ea
iamman307 <iamman307@gmail.com>
2020-12-17 22:44:37 +0800

    first

---
+++ app/src/main/java/com/example/servicekl/MainActivity2.kt
@@ -0,0 +1,12 @@
+package com.example.servicekl
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.servicekl.R
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
